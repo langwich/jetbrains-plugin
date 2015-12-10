dc7198f341a090858d9b2b514e5c2bbf6812c0b7
Terence Parr <parrt@antlr.org>
2015-12-09 17:09:54 -0800

    shift wich config to prefs area.

--- src/java/org/antlr/jetbrains/wichplugin/actions/ConfigureWichAction.java
+++ src/java/org/antlr/jetbrains/wichplugin/actions/ConfigureWichAction.java
@@ -2,14 +2,7 @@ package org.antlr.jetbrains.wichplugin.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.vfs.VirtualFile;
-import org.antlr.jetbrains.wichplugin.WichPluginController;
-import org.antlr.jetbrains.wichplugin.dialogs.WichConfigDialog;
 
 public class ConfigureWichAction extends AnAction {
 	public void actionPerformed(AnActionEvent e) {
@@ -17,25 +10,25 @@ public class ConfigureWichAction extends AnAction {
 		if ( project==null ) {
 			return; // whoa!
 		}
-		WichConfigDialog configDialog = new WichConfigDialog(project);
-		configDialog.setTitle("Configure Wich Plugin");
-		configDialog.show();
-
-		if ( configDialog.getExitCode()==DialogWrapper.OK_EXIT_CODE ) {
-			configDialog.saveValues(project);
-
-			WichPluginController controller = WichPluginController.getInstance(project);
-			if ( controller!=null ) {
-				FileEditorManager fmgr = FileEditorManager.getInstance(project);
-				VirtualFile[] files = fmgr.getSelectedFiles();
-				for (int i = 0; i<files.length; i++) {
-					VirtualFile file = files[i];
-					if ( file.getName().endsWith(".w") ) {
-						final Document doc = FileDocumentManager.getInstance().getDocument(file);
-						controller.editorDocumentAlteredEvent(doc);
-					}
-				}
-			}
-		}
+//		WichConfigDialog configDialog = new WichConfigDialog(project);
+//		configDialog.setTitle("Configure Wich Plugin");
+//		configDialog.show();
+//
+//		if ( configDialog.getExitCode()==DialogWrapper.OK_EXIT_CODE ) {
+//			configDialog.saveValues(project);
+//
+//			WichPluginController controller = WichPluginController.getInstance(project);
+//			if ( controller!=null ) {
+//				FileEditorManager fmgr = FileEditorManager.getInstance(project);
+//				VirtualFile[] files = fmgr.getSelectedFiles();
+//				for (int i = 0; i<files.length; i++) {
+//					VirtualFile file = files[i];
+//					if ( file.getName().endsWith(".w") ) {
+//						final Document doc = FileDocumentManager.getInstance().getDocument(file);
+//						controller.editorDocumentAlteredEvent(doc);
+//					}
+//				}
+//			}
+//		}
 	}
 }
